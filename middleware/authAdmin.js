95ce1d82aa4d0d8f178e3803e1b786d24818a9ac
nextgen-2025 <203623776+nextgen-2025@users.noreply.github.com>
2025-05-24 11:20:00 +0530

    add proper error names on both

--- middleware/authAdmin.js
+++ middleware/authAdmin.js
@@ -9,7 +9,7 @@ const authAdmin = async (req, res, next) => {
         }
         const token_decode = jwt.verify(atoken, process.env.JWT_SECRET)
         if (token_decode !== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD) {
-            return res.json({ success: false, message: 'Not Authorized Login Again' })
+            return res.json({ success: false, message: 'Not Authorized Admin Login Again' })
         }
         next()
     } catch (error) {
--- middleware/authUser.js
+++ middleware/authUser.js
@@ -4,7 +4,7 @@ import jwt from 'jsonwebtoken'
 const authUser = async (req, res, next) => {
     const { token } = req.headers
     if (!token) {
-        return res.json({ success: false, message: 'Not Authorized Login Again' })
+        return res.json({ success: false, message: 'Not Authorized Frontend Login Again' })
     }
     try {
         const token_decode = jwt.verify(token, process.env.JWT_SECRET)
@@ -16,7 +16,7 @@ const authUser = async (req, res, next) => {
         next()
     } catch (error) {
         console.log(error)
-        res.json({ success: false, message: error.message })
+        res.status(401).json({ success: false, message: error.message })
     }
 }
 
